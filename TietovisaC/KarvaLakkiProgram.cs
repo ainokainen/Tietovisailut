c244238505cf8b207c031da81c31e6956c3bd3dd
ainokainen <aino.rouvinen@gmail.com>
2018-09-19 14:16:24 +0300

    Add project files.

---
+++ TietovisaC/KarvaLakkiProgram.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TietovisaC
+{
+    class KarvaLakkiProgram
+    {
+        static void Main(string[] args)
+        {
+            Kysymys kysymys = new Kysymys();
+            string vastaus = "";
+            int pisteet = 0;
+            string oikeaVastaus = "";
+            int kysymystenLkm = 0;
+
+            Console.WriteLine("Tervetuloa Tietovisaan! Kysyn 10 kysymystä");
+            Console.WriteLine("Vastaa annettuihin kysymyksiin K tai E");
+            Console.WriteLine("****************************************");
+
+            for (int i = 0; i < 10; i++)
+            {
+                string[]kysVas= kysymys.KysyKysymys().Split('*');
+                Console.WriteLine(kysVas[0]);
+                Console.Write("Vastaus: ");
+
+                vastaus = Console.ReadLine().ToUpper();
+
+                oikeaVastaus = kysVas[1];
+                //Vastauksen arviointi
+                if (vastaus==oikeaVastaus)                  
+                {
+                    pisteet++;
+                    kysymystenLkm++;
+                    Console.WriteLine("Oikea vastaus!");
+                }
+                else
+                {
+                    kysymystenLkm++;
+                    Console.WriteLine("Vastaus väärin :(");
+                }
+                Console.WriteLine("Vastattuja kysymyksiä " + kysymystenLkm+ "/10.");
+                Console.WriteLine("___________________________");
+        
+    }
+
+            Console.WriteLine("Pistemääräsi on: " + pisteet);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
+
---
+++ TietovisaC/Kysymys.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TietovisaC
+{
+    public class Kysymys
+    {
+        private string kysymysTeksti;
+        private List<String> vastausVaihtoehdot;
+        private int oikeanVaihtoehdonIndeksi;
+        public string KysymysTeksti { get => kysymysTeksti; set => kysymysTeksti = value; }
+        public int OikeanVaihtoehdonIndeksi { get => oikeanVaihtoehdonIndeksi; set => oikeanVaihtoehdonIndeksi = value; }
+
+        //public SortedList VastausVaihtoehdot = new SortedList();
+
+        public string KysyKysymys()
+        {
+            string[] kysymykset = File.ReadAllLines(@"C:\Users\ainon\OneDrive\Desktop\C# ohjelmointi\Viikko_1\TietovisaC\TietovisaC\Kysymykset.txt");
+
+
+            Random r = new Random();
+
+            for (int i = kysymykset.Length; i > 1;)
+            {
+                int k = r.Next(i--);
+                string temp = kysymykset[i];
+                kysymykset[i] = kysymykset[k];
+                kysymykset[k] = temp;
+            }
+
+            //foreach (var kysymys in kysymykset)
+            //{
+            //    Console.WriteLine(kysymys);
+            //}
+
+            
+            return kysymykset[0];
+            
+
+            //return "_________________________________________________";
+
+
+            
+        }
+       
+    }
+}
